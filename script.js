1075cb64404dbc46b8f45a86e42b0a9e41bbd9f0
Jovaun_Henriques <jovaun@hotmail.com>
2023-10-05 15:40:36 -0230

    i did alot

--- script.js
+++ script.js
@@ -2,15 +2,39 @@
 // Get the <a> element by its ID
 let wtf = document.getElementById("wtf");
 
-// This is the desired URL for the home page
-let destinationURL = "./index.html";
+// This is the desired URL for your home page
+let homePageURL = "index.html";
 
-if (window.location.href.endsWith("index.html")) {
-  wtf.addEventListener("click", function() {
-    window.location.href = destinationURL
-  })
+// Check if the current page's URL matches the home page URL
+if (window.location.href.endsWith(homePageURL)) {
+  // If on the home page, add the click event listener
+  wtf.addEventListener("click", function(event) {
+    let destinationURL = "./topFreezer.html";
+    window.location.href = destinationURL;
+
+    // Remove the click event listener to make the image non-clickable
+    wtf.removeEventListener("click", handleClick);
+  });
+
+  function handleClick(event) {
+    event.preventDefault(); // Prevent the default link behavior
+  }
+
+  // Add the initial click event listener
+  wtf.addEventListener("click", handleClick);
+} else {
+  // If not on the home page, disable the link completely
+  wtf.removeAttribute("href");
+  wtf.style.cursor = "default";
 }
-//this is for the contactpage
+
+
+//this is where the js for the contactpage starts.
+let contact =document.getElementById("contact");
+
+// link to the the contact page.
+let destinationURl2 = "./contactpage"
+
 document.getElementById("contact-form").addEventListener("submit", function(event) {
   event.preventDefault(); // Prevent the default form submission
 
