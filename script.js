7cdcceaa8d6be09ed6eadd70ff00618a509da799
Jovaun_Henriques <jovaun@hotmail.com>
2023-10-08 15:44:39 -0230

    Fixing issues

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
 
-// Get the <a> element by its ID
-let wtf = document.getElementById("wtf","wfd","flw","ed");
+// Get the <a> elements by their respective IDs
+let wtf = document.getElementById("wtf");
+let wfd = document.getElementById("wfd");
+let flw = document.getElementById("flw");
+let ed = document.getElementById("ed");
 
 // This is the desired URL for your home page
 let homePageURL = "index.html";
 
+// Function to handle the click event for the home page links
+function handleClick(event) {
+  let destinationURL = "./topFreezer.html";
+  window.location.href = destinationURL;
+  // Remove the click event listener to make the image non-clickable
+  event.target.removeEventListener("click", handleClick);
+}
+
 // Check if the current page's URL matches the home page URL
-if (window.location.href.endsWith("./index.html")) {
-  // If on the home page, add the click event listener
-  wtf.addEventListener("click", function(event) {
-    let destinationURL = "./topFreezer.html";
-    window.location.href = destinationURL;
-    // Remove the click event listener to make the image non-clickable
-    
-    wtf.removeEventListener("click", handleClick);
-    
-  })}
+if (window.location.href.endsWith(homePageURL)) {
+  // If on the home page, add the click event listeners
+  wtf.addEventListener("click", handleClick);
+  wfd.addEventListener("click", handleClick);
+  flw.addEventListener("click", handleClick);
+  ed.addEventListener("click", handleClick);
+}
 
+// For the contact page
+let contact = document.getElementById("contact");
 
-//this is where the js for the contactpage starts.
-let contact =document.getElementById("contact");
-
- contact.addEventListener(click,function(){
-   window.open("./contactpage.html")
- });
+contact.addEventListener("click", function() {
+  window.open("./contactpage.html");
+});
 
 document.getElementById("contact-form").addEventListener("submit", function(event) {
   event.preventDefault(); // Prevent the default form submission
@@ -35,8 +42,8 @@ document.getElementById("contact-form").addEventListener("submit", function(even
 
   // Simple validation: Check if any field is empty
   if (!name || !email || !message) {
-      alert("Please fill in all fields.");
-      return;
+    alert("Please fill in all fields.");
+    return;
   }
 
   // If all fields are filled, you can proceed with sending the data to your server or another action.
